1c19d024aed8690e0ffad5933eb2a445e2e9d04f
dani <danielabesha7@gmail.com>
2023-03-13 01:53:00 -0700

    cgqedc

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    new_string = my_string.translate({ord(i): None for i in 'cC'})
-    return new_string
 
+def no_c(my_string):
+    chars = len(my_string)
+    buf = ""
+    for i in range(0, chars):
+        if my_string[i] == "c" or my_string[i] == "C":
+            continue
+        buf = buf + my_string[i]
+    return buf
