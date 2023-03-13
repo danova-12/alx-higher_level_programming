ebd9c5e8f832dbcac6e4eb20a150cb599a673296
dani <danielabesha7@gmail.com>
2023-03-13 01:00:50 -0700

    gwei

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    '''python program prints items passed'''
-    for i  in my_list:
-        print('{:d}'.format(i))
-
+    for my_list in my_list:
+        print("{}".format(my_list))
