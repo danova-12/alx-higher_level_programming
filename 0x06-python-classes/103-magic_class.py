b39bfd0adb7f0c80ca32ab7ec1560235c71af44c
dani <danielabesha7@gmail.com>
2023-03-28 04:45:17 -0700

    ugwwug

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 103-magic_calculation.py
+
 """Define a MagicClass matching exactly a bytecode provided by Holberton."""
 
 import math
@@ -25,4 +25,3 @@ class MagicClass:
     def circumference(self):
         """Return The circumference of the MagicClass."""
         return (2 * math.pi * self.__radius)
-
