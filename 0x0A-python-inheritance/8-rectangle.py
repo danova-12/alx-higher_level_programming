598ac10019cb37debca9de5412c620d04544ceff
dani <danielabesha7@gmail.com>
2023-04-10 13:58:31 -0700

    icreuhf

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle that inherits from BaseGeometry."""
+"""Module that holds the class Rectangle
+"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Represent  BaseGeometry inheret from rectangle"""
+    """Inherits from BaseGeometry
+    """
 
     def __init__(self, width, height):
-        """Intialize a new Rectangle.
-        Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
+        """Initializes the class
+            Args:
+                width (int): width rectangle
+                height (int): height rectangle
         """
+
         self.integer_validator("width", width)
         self.__width = width
+
         self.integer_validator("height", height)
         self.__height = height
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,29 +1,59 @@
 #!/usr/bin/python3
-"""Defines a class Rectangle that inherits from BaseGeometry."""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Module that holds the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry class
+    """
+
+    def area(self):
+        """Calculates the area
+            Returns:
+                float: area
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates input
+        """
+
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
-    """Represent a rectangle using BaseGeometry."""
+    """Inherits from BaseGeometry
+    """
 
     def __init__(self, width, height):
-        """Intialize a new Rectangle.
-        Args:
-            width (int): The width of the new Rectangle.
-            height (int): The height of the new Rectangle.
+        """Initializes the class
+            Args:
+                width (int): width rectangle
+                height (int): height rectangle
         """
-        super().integer_validator("width", width)
+
+        self.integer_validator("width", width)
         self.__width = width
-        super().integer_validator("height", height)
+
+        self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """Return the area of the rectangle."""
+        """Calculates area for a rectangle
+            Returns:
+                float: area rectangle
+        """
+
         return self.__width * self.__height
 
     def __str__(self):
-        """Return the print() and str() representation of a Rectangle."""
-        string = "[" + str(self.__class__.__name__) + "] "
-        string += str(self.__width) + "/" + str(self.__height)
-        return string
+        """Returns rectangle represention
+            Returns:
+                str: rectangle representation
+        """
 
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
